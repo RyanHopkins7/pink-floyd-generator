fd01e21f35c0e88234f4d47b91bb442683ca8e72
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-10-28 16:00:52 -0700

    use dict_hist function, use word .lower to not take into account casing

--- histogram.py
+++ histogram.py
@@ -1,30 +1,24 @@
 def dict_histogram(words):
     hist = {}
     for word in words:
-        if word in hist:
-            hist[word] += 1
+        if word.lower() in hist:
+            hist[word.lower()] += 1
         else:
-            hist[word] = 1
-    return hist
+            hist[word.lower()] = 1
+
+    # Sort dict by values to see most frequently occuring words
+    sorted_word_list = sorted(list( zip(hist.values(), hist.keys()) ))
+    sorted_word_list = {keyword: num_occur for num_occur, keyword in sorted_word_list}
+
+    return sorted_word_list
 
 def list_histogram(words):
-    hist = {}
-    for word in words:
-        if word in hist:
-            hist[word] += 1
-        else:
-            hist[word] = 1
-    return list(map(list, hist.items()))
+    return list(map(list, dict_histogram(words).items()))
 
 def tuple_histogram(words):
-    hist = {}
-    for word in words:
-        if word in hist:
-            hist[word] += 1
-        else:
-            hist[word] = 1
-    return hist.items()
+    return dict_histogram(words).items()
 
 with open('theparochialhistoryofcornwall.txt') as f:
     words = f.read().split(' ')
+    print(list_histogram(words))
 
