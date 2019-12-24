1540afeffb8e3a7d1617108f136dc4019e4b7f70
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-12-24 11:27:13 -0500

    Remove unnecessary init memory

--- markov_model.py
+++ markov_model.py
@@ -5,25 +5,24 @@ class MarkovModel(dict):
     """ Dictionary based nth order markov model """
 
     def __init__(self, corpus=[], order=1):
-        # init_memory used for initializing markov model and adding states
-        self.init_memory = Memory(order)
-        # memory used for sampling from markov model
         self.memory = Memory(order)
 
         # Adding states spliced before order allows model to loop to beginning once the end is reached when sampling
         for message in corpus + corpus[:order]:
             self.add_state(message)
 
+        self.memory.clear()
+
     def add_state(self, new_state):
         """ Add a state to markov model and add new state to init_memory """
-        current_state = self.init_memory.serialize()
+        current_state = self.memory.serialize()
         
         if current_state in self:
             self[current_state].append(new_state)
         else:
             self[current_state] = [new_state]
 
-        self.init_memory.enqueue(new_state)
+        self.memory.enqueue(new_state)
 
     def sample(self, N=1, starting_state=tuple()):
         """ Return generator from sampling N times from markov model """
@@ -35,6 +34,7 @@ class MarkovModel(dict):
             self.memory.enqueue(next_state)
             yield next_state
             starting_state = self.memory.serialize()
+            
         self.memory.clear()
 
 if __name__ == '__main__':
